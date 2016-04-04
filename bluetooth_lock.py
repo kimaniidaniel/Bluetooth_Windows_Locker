589498970da77c4190e5c3117f71e6c534d0bf6a
Kimanii Daniel <kimanii.daniel@superfund.com>
2016-04-04 12:06:02 -0400

    added a note about pairing your device first

--- bluetooth_lock.py
+++ bluetooth_lock.py
@@ -4,20 +4,21 @@ import ctypes
 """
 To use this tool change the your_device_bluetooth_name to
 your device blueooth name - you should be able to find this
-in your device's bluetooth settings
+in your device's bluetooth settings.
+You MUST first pair your device for this to work.
 
 Requirements:
  1. Install PyBluez - https://pypi.python.org/pypi/PyBluez
 """
 #--------------Change Here--------------
-your_device_bluetooth_name = "Xperia Z2"
+your_device_bluetooth_name = "Galaxy S6 edge+"
 #---------------------------------------
 
 
 nearby_devices = []
 lock = True
 try:
-    nearby_devices = bluetooth.discover_devices(duration=10, lookup_names = True, flush_cache=True, lookup_class=False)
+    nearby_devices = bluetooth.discover_devices(duration=8, lookup_names = True, flush_cache=True, lookup_class=False)
 except:
     print "Issue with bluetooth lookup, halting"
 
